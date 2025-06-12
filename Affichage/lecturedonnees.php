ed24c0192ee8080341b99a6119a964f44c613359
Mae3101 <maevachloee@gmail.com>
2025-06-12 11:00:24 +0200

    lire les données de la carte

---
+++ Affichage/lecturedonnees.php
@@ -0,0 +1,13 @@
+<?php
+try{
+  $pdo = new PDO ('mysql:host=localhost;dbname=bdd;charset=utf8','root','');
+  $stmt=$pdo->query("SELECT* FROM G7E ORDER BY Time DESC LIMIT 1");
+  $row= $stmt->fetch(PDO::FETCH_ASSOC);
+  echo json_encode([
+    'degrees'=>$row['Degrees'],
+    'time'=>$row['Time']
+   ]);
+} catch (PDOException $e){
+   echo json_ecode(['error'=> 'Erreur de connexion']);
+}
+?>
